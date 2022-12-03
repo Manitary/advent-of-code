c3686cdeb7d0dfe105e0e017c4156818f428babe
Manitary <pak.ska@gmail.com>
2022-12-03 06:47:35 +0000

    Improve readability 3/2022

--- 2022/python/03.py
+++ 2022/python/03.py
@@ -1,5 +1,6 @@
 from aocd import get_data, submit
 from typing import Iterable
+from itertools import starmap
 
 def itemPriority(char: str) -> int:
     return 1 + ord(char) - ord('a') if char.islower() else 27 + ord(char) - ord('A')
@@ -7,10 +8,13 @@ def itemPriority(char: str) -> int:
 def groupPriority(*string_list: Iterable[str]) -> int:
     return itemPriority(set.intersection(*(set(x) for x in string_list)).pop())
 
+def stringPriority(string: str) -> int:
+    return groupPriority(string[:len(string)//2], string[len(string)//2:])
+
 def main() -> tuple[int, int]:
     data = get_data(day=3, year=2022).split()
-    ans1 = sum(map(lambda x: groupPriority(x[:len(x)//2], x[len(x)//2:]), data))
-    ans2 = sum(map(lambda x: groupPriority(*x), zip(data[::3], data[1::3], data[2::3])))
+    ans1 = sum(map(stringPriority, data))
+    ans2 = sum(starmap(groupPriority, zip(data[::3], data[1::3], data[2::3])))
     return ans1, ans2
 
 if __name__ == "__main__":

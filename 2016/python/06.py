520ebfe32e90f13a6f841bf4cdb76a476d4a9b5d
Manitary <pak.ska@gmail.com>
2022-12-06 11:52:04 +0000

    06/2016 use enumerate

--- 2016/python/06.py
+++ 2016/python/06.py
@@ -5,10 +5,10 @@ YEAR = 2016
 
 data = get_data(day=DAY, year=YEAR).split()
 
-count = [Counter() for _ in range(len(data[0]))]
+count = [Counter() for _ in enumerate(data[0])]
 for row in data:
-    for i in range(len(row)):
-        count[i][row[i]] += 1
+    for i, char in enumerate(row):
+        count[i][char] += 1
 
 ans1 = ''.join([c.most_common(1)[0][0] for c in count])
 ans2 = ''.join([c.most_common()[-1][0] for c in count])

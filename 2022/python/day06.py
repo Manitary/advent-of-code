8bcdc6f283187eca71e93b3ad33ed6c0fc2aafb8
Manitary <pak.ska@gmail.com>
2022-12-06 07:46:06 +0000

    Refactor 06/2022

--- 2022/python/day06.py
+++ 2022/python/day06.py
@@ -2,16 +2,21 @@
 
 from aocd import get_data, submit
 
+def find_marker(string: str, length: int, start: int = 0) -> tuple[int, int]:
+    """"
+    Return the first and last index of the start-of-packet marker of given length.
+    The search begins from the given start index.
+    """
+    for i in range(start, len(string) - length):
+        if len(set(string[i:i + length])) == length:
+            return i, i + length
+    return None
+
 def main() -> tuple[int, int]:
     """Return the answers to part 1 and part 2."""
-    part1, part2 = None, None
     data = get_data(day=6, year=2022)
-    for i, _ in enumerate(data):
-        if (not part1) and len(set(data[i:i+4])) == 4:
-            part1 = i + 4
-        if (not part2) and len(set(data[i:i+14])) == 14:
-            part2 = i + 14
-            break
+    start, part1 = find_marker(data, 4)
+    _, part2 = find_marker(data, 14, start)
 
     return part1, part2
 

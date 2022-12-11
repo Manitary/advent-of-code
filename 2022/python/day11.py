8407603d193abb8d8b8f14d8d17dfcc4feca178b
Manitary <pak.ska@gmail.com>
2022-12-11 11:14:29 +0000

    11/2022 decouple and reduce overhead

--- 2022/python/day11.py
+++ 2022/python/day11.py
@@ -1,108 +1,111 @@
 """Solve Advent of Code Day 11 Year 2022."""
 
+from __future__ import annotations
 import re
 from dataclasses import dataclass, field
-from collections import deque
 from copy import deepcopy
 from math import prod, lcm
-from functools import cached_property
 from aocd import get_data, submit
 
 PATTERN_OP = r"Operation: new = ((?:old\s?|\*\s?|\+\s?|\d+\s?)+)$"
 re.compile(PATTERN_OP)
 
+
 @dataclass
 class Monkey:
+    """A monkey.
+
+    Attributes:
+        items: a list of items held by the monkey.
+        operation: the operation applied to the item when the monkey inspects it.
+        worry_control: the operaction applied to the item after the monkey inspected it.
+        test_value: the number used for divisibility check.
+        target_true: the target monkey (or its number) if test_value divides the item.
+        target_false: the target monkey (or its number) if test_value does not divide the item.
+        inspections: the number of inspections done by the monkey.
+    """
+
     operation: callable
-    test: int
-    true: int
-    false: int
-    worry_control: callable = lambda x: x//3
-    items: deque[int] = field(default_factory=deque)
+    test_value: int
+    target_true: int | Monkey
+    target_false: int | Monkey
+    worry_control: callable = lambda x: x // 3
+    items: list[int] = field(default_factory=list)
     inspections: int = field(default=0, init=False)
 
-    def clear_items(self):
+    def play(self) -> None:
+        """Make the monkey inspect and throw all of its items according to its rules."""
+        for item in self.items:
+            new_item = self.worry_control(self.operation(item))
+            if new_item % self.test_value:
+                self.target_false.items.append(new_item)
+            else:
+                self.target_true.items.append(new_item)
+        self.inspections += len(self.items)
         self.items = []
 
-    def grab_item(self):
-        return self.items.popleft()
 
-    def obtain_item(self, item):
-        self.items.append(item)
+def parse_monkey(monkey_data: str) -> Monkey:
+    """Return a monkey corresponding to the given data.
 
-    def set_worry_control(self, worry_control):
-        self.worry_control = worry_control
+    Input format:
+    "Monkey [number]:
+        Starting items: [int], ..., [int]
+        Operation: new = [function(old)]
+        Test: divisible by [int]
+        If true: throw to monkey [int]
+        If false: throw to monkey [int]"
+    """
+    monkey_data = monkey_data.split("\n")
+    starting_items = list(map(int, re.findall(r"(\d+)", monkey_data[1])))
+    operation = eval("lambda old: " + re.findall(PATTERN_OP, monkey_data[2])[0])
+    test_value = int(monkey_data[3].split()[-1])
+    target_true = int(monkey_data[4].split()[-1])
+    target_false = int(monkey_data[5].split()[-1])
+    return Monkey(
+        items=starting_items,
+        operation=operation,
+        test_value=test_value,
+        target_true=target_true,
+        target_false=target_false,
+    )
 
-    def inspect(self):
-        item = self.worry_control(self.operation(self.grab_item()))
-        self.inspections += 1
-        target_num = self.false if item % self.test else self.true
-        return item, target_num
 
-    def play(self):
-        while self.items:
-            yield self.inspect()
+def monkey_business(zoo: list[Monkey]) -> int:
+    """Return the level of monkey business of a list of monkeys.
 
-@dataclass
-class Zoo:
-    _monkeys: list[Monkey] = field(default_factory=list)
+    The level of monkey business is the product of the number of inspections
+    done by the two most active monkeys.
+    """
+    return prod(sorted(monkey.inspections for monkey in zoo)[-2:])
 
-    def __getitem__(self, num: int):
-        return self._monkeys[num]
 
-    @cached_property
-    def total_worry(self):
-        return lcm(*(monkey.test for monkey in self._monkeys))
+def monkey_play(zoo: list[Monkey], num: int) -> int:
+    """Let the monkeys in a given list play a given number of times."""
+    for _ in range(num):
+        for monkey in zoo:
+            monkey.play()
 
-    def set_worry_control(self, worry_control):
-        for monkey in self._monkeys:
-            monkey.set_worry_control(worry_control)
 
-    def addMonkey(self, monkey_data: str):
-        monkey_data = monkey_data.split('\n')
-        starting_items = deque(map(int, re.findall(r"(\d+)", monkey_data[1])))
-        operation = eval("lambda old: " + re.findall(PATTERN_OP, monkey_data[2])[0])
-        test = int(monkey_data[3].split()[-1])
-        true = int(monkey_data[4].split()[-1])
-        false = int(monkey_data[5].split()[-1])
-        new_monkey = Monkey(items=starting_items, operation=operation, test=test, true=true, false=false)
-        self._monkeys.append(new_monkey)
-
-    def play(self):
-        for monkey in self._monkeys:
-            for item, target in monkey.play():
-                self[target].obtain_item(item)
-
-    @property
-    def inspections(self):
-        return (monkey.inspections for monkey in self._monkeys)
-
-    @property
-    def monkey_business(self):
-        return prod(sorted(self.inspections)[-2:])
-
-def main():
-    data = get_data(day=11, year=2022)
-    data = data.split('\n\n')
-
-    zoo_1 = Zoo()
-    for monkey in data:
-        zoo_1.addMonkey(monkey)
-
+def main() -> tuple[int, int]:
+    """Return the solution to part 1 and part 2."""
+    data = get_data(day=11, year=2022).split("\n\n")
+    zoo_1 = [parse_monkey(monkey_data) for monkey_data in data]
+    for monkey in zoo_1:
+        monkey.target_false = zoo_1[monkey.target_false]
+        monkey.target_true = zoo_1[monkey.target_true]
     zoo_2 = deepcopy(zoo_1)
-
-    for _ in range(20):
-        zoo_1.play()
-
-    part1 = zoo_1.monkey_business
-
-    zoo_2.set_worry_control(lambda x: x % zoo_2.total_worry)
-
-    for _ in range(10000):
-        zoo_2.play()
-
-    part2 = zoo_2.monkey_business
+    # Part 1
+    monkey_play(zoo=zoo_1, num=20)
+    part1 = monkey_business(zoo_1)
+    # Part 2
+    total_worry = lcm(*(monkey.test_value for monkey in zoo_2))
+    for monkey in zoo_2:
+        monkey.worry_control = lambda x: x % total_worry
+    monkey_play(zoo=zoo_2, num=10000)
+    part2 = monkey_business(zoo_2)
     return part1, part2
+
 
 if __name__ == "__main__":
     ans1, ans2 = main()

6ecabfef822f8e65dee44e9d2136b76ad04aae1f
Manitary <pak.ska@gmail.com>
2023-03-27 16:55:32 +0100

    Use built-in modular inverse

--- 2016/python/15.py
+++ 2016/python/15.py
@@ -1,6 +1,6 @@
 from aocd import get_data, submit
 from functools import reduce
-from re import findall, compile
+import re
 
 DAY = 15
 YEAR = 2016
@@ -10,35 +10,19 @@ data = data.split("\n")
 
 
 def chinese_remainder(n, a):
-    sum = 0
+    ans = 0
     prod = reduce(lambda a, b: a * b, n)
     for n_i, a_i in zip(n, a):
         p = prod // n_i
-        sum += a_i * mul_inv(p, n_i) * p
-    return sum % prod
+        ans += a_i * pow(p, -1, n_i) * p
+    return ans % prod
 
 
-def mul_inv(a, b):
-    b0 = b
-    x0, x1 = 0, 1
-    if b == 1:
-        return 1
-    while a > 1:
-        q = a // b
-        a, b = b, a % b
-        x0, x1 = x1 - q * x0, x0
-    if x1 < 0:
-        x1 += b0
-    return x1
-
-
-pattern = r" (\d+)"
-compile(pattern)
-
+pattern = re.compile(r" (\d+)")
 mods = []
 residues = []
 for i, row in enumerate(data):
-    total, start = tuple(map(int, findall(pattern, row)))
+    total, start = tuple(map(int, pattern.findall(row)))
     mods.append(total)
     residues.append(total - (start + i + 1))
 

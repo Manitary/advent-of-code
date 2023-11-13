63f0bfd0a925bdaa3432b7fdb852037323904f8d
Manitary <pak.ska@gmail.com>
2023-11-13 19:20:51 +0000

    Update 2015/14 Python
    Use namedtuple over dict
    Instead of simulating, use the closed formula at every step

--- 2015/python/day14.py
+++ 2015/python/day14.py
@@ -1,4 +1,5 @@
 import re
+from typing import NamedTuple
 
 from aocd import get_data, submit
 
@@ -8,48 +9,31 @@ YEAR = 2015
 TIME_LIMIT = 2503
 RE_REINDEER = re.compile(r"\d+")
 
+Reindeer = NamedTuple(
+    "Reindeer", [("speed", int), ("max_time_flying", int), ("max_time_resting", int)]
+)
 
-def pos(reindeer: dict[str, int], elapsed: int) -> int:
-    s, ft, rt = (
-        reindeer["speed"],
-        reindeer["max_time_flying"],
-        reindeer["max_time_resting"],
-    )
-    return s * (elapsed // (ft + rt) * ft + min(ft, elapsed % (ft + rt)))
+
+def pos(reindeer: Reindeer, elapsed: int) -> int:
+    s, ft, rt = reindeer.speed, reindeer.max_time_flying, reindeer.max_time_resting
+    f1, f2 = divmod(elapsed, ft + rt)
+    return s * (f1 * ft + min(ft, f2))
 
 
 def main() -> tuple[int, int]:
     data = get_data(day=DAY, year=YEAR).split("\n")
-    reindeers: dict[int, dict[str, int]] = {}
-    for i, row in enumerate(data):
-        s, ft, rt = map(int, tuple(RE_REINDEER.findall(row)))
-        reindeers[i] = {
-            "speed": s,
-            "max_time_flying": ft,
-            "max_time_resting": rt,
-            "time_flying": 0,
-            "time_resting": 0,
-        }
-
-    part1 = max(iter(pos(reindeer, TIME_LIMIT) for reindeer in reindeers.values()))
-
-    distance = [0] * len(reindeers)
-    score = [0] * len(reindeers)
-    for _ in range(TIME_LIMIT):
-        for i, r in reindeers.items():
-            if r["time_flying"] < r["max_time_flying"]:
-                r["time_flying"] += 1
-                distance[i] += r["speed"]
-            else:
-                r["time_resting"] += 1
-                if r["time_resting"] == r["max_time_resting"]:
-                    r["time_flying"] = 0
-                    r["time_resting"] = 0
-        best = max(distance)
-        for i, d in enumerate(distance):
-            if d == best:
-                score[i] += 1
-    part2 = max(score)
+    reindeers = tuple(
+        Reindeer(*map(int, tuple(RE_REINDEER.findall(row)))) for row in data
+    )
+    part1 = max(iter(pos(reindeer, TIME_LIMIT) for reindeer in reindeers))
+    scores = [0] * len(reindeers)
+    for i in range(1, TIME_LIMIT + 1):
+        dists = tuple(pos(reindeer, i) for reindeer in reindeers)
+        best = max(dists)
+        for j, dist in enumerate(dists):
+            if dist == best:
+                scores[j] += 1
+    part2 = max(scores)
 
     return part1, part2
 

9aef511553b65256048c90f13f317ec3f5cd4121
Manitary <pak.ska@gmail.com>
2023-12-25 07:05:53 +0000

    Update 2023/22
    Faster fall simulation
    Faster p2 by not having to copy the graphs for each iteration; instead, make a new set that tracks removed blocks.

--- 2023/python/day22.py
+++ 2023/python/day22.py
@@ -1,6 +1,3 @@
-import copy
-import functools
-import itertools
 import re
 from collections import defaultdict
 
@@ -9,96 +6,87 @@ from aocd import get_data, submit
 DAY = 22
 YEAR = 2023
 
-type Coord = tuple[int, int, int]
+type Coord2D = tuple[int, int]
+type Coord3D = tuple[int, int, int]
+type Brick = set[Coord3D]
+type Graph = dict[int, set[int]]
 
 COORD_RE = re.compile(r"\d+")
 
 
-def parse_block(block_data: str) -> set[Coord]:
-    x1, y1, z1, x2, y2, z2 = map(int, COORD_RE.findall(block_data))
-    if x1 != x2:
-        return {(x, y1, z1) for x in range(min(x1, x2), max(x1, x2) + 1)}
-    if y1 != y2:
-        return {(x1, y, z1) for y in range(min(y1, y2), max(y1, y2) + 1)}
-    if z1 != z2:
-        return {(x1, y1, z) for z in range(min(z1, z2), max(z1, z2) + 1)}
-    return {(x1, y1, z1)}
+def parse_brick(brick_data: str) -> Brick:
+    """Return the set of brick pieces.
+
+    Only keep track of min/max height:
+    if they are different, it means it is a vertical block, so they are
+    the only parts of the block relevant to calculate the falling behaviour.
+    """
+    x1, y1, z1, x2, y2, z2 = map(int, COORD_RE.findall(brick_data))
+    return {
+        (x, y, z)
+        for x in range(min(x1, x2), max(x1, x2) + 1)
+        for y in range(min(y1, y2), max(y1, y2) + 1)
+        for z in (z1, z2)
+    }
 
 
-def can_fall(occupied: set[Coord], block: set[Coord]) -> int:
-    h = min(x[2] for x in block)
-    if h == 1:
-        return 0
-    other = occupied - block
-    for i in range(1, h):
-        if any((x[0], x[1], x[2] - i) in other for x in block):
-            return i - 1
-    return h - 1
+def make_support_graph(bricks: list[Brick]) -> tuple[Graph, Graph]:
+    xyz_to_brick: dict[Coord3D, int] = {}
+    xy_highest_z: dict[Coord2D, int] = {}
+    supports: Graph = defaultdict(set)
+    supported: Graph = defaultdict(set)
+
+    for i, brick in enumerate(bricks):
+        fall_height = min(z - xy_highest_z.get((x, y), 0) - 1 for x, y, z in brick)
+        fallen_brick = {(x, y, z - fall_height) for x, y, z in brick}
+        for tile_under in ((x, y, z - 1) for x, y, z in fallen_brick):
+            if brick_under := xyz_to_brick.get(tile_under):
+                supports[brick_under].add(i)
+                supported[i].add(brick_under)
+
+        for x, y, z in fallen_brick:
+            xyz_to_brick[x, y, z] = i
+            xy_highest_z[x, y] = max(z, xy_highest_z.get((x, y), z))
+
+        bricks[i] = fallen_brick
+
+    return supports, supported
 
 
-def _count_falling(
-    to_remove: int, supports: dict[int, set[int]], supported: dict[int, set[int]]
-) -> int:
-    ss = copy.deepcopy(supports)
-    sd = copy.deepcopy(supported)
-    to_process = {to_remove}
-    count = 0
-    while to_process:
-        curr = to_process.pop()
-        count += 1
-        to_check = ss.pop(curr, set())
-        for i in to_check:
-            if i not in sd:
-                continue
-            sd[i].discard(curr)
-            if not sd[i]:
-                sd.pop(i)
-                to_process.add(i)
-    return count - 1
+def num_falling_bricks(brick_to_remove: int, supports: Graph, supported: Graph) -> int:
+    fell = 0
+    supports_lost = {brick_to_remove}
+    todo = supports[brick_to_remove]
+
+    while todo:
+        brick = todo.pop()
+        if brick in supports_lost:
+            continue
+
+        if supported.get(brick, set()).issubset(supports_lost):
+            fell += 1
+            supports_lost.add(brick)
+            todo |= supports.get(brick, set())
+
+    return fell
 
 
 def main() -> tuple[int, int]:
     data = get_data(day=DAY, year=YEAR).splitlines()
 
-    blocks = sorted(map(parse_block, data), key=lambda x: min(b[2] for b in x))
-
-    occupied = set[Coord].union(*itertools.chain(blocks))
-
-    while True:
-        fell = False
-        for i, b in enumerate(blocks):
-            if h := can_fall(occupied, b):
-                occupied -= b
-                blocks[i] = {(x[0], x[1], x[2] - h) for x in b}
-                occupied |= blocks[i]
-                fell = True
-        if not fell:
-            break
-
-    blocks.sort(key=lambda x: min(b[2] for b in x))
-
-    supports: dict[int, set[int]] = defaultdict(set)
-    for i1, i2 in itertools.combinations(range(len(blocks)), 2):
-        b1 = blocks[i1]
-        b2 = blocks[i2]
-        if any((t[0], t[1], t[2] - 1) in b1 for t in b2):
-            supports[i1].add(i2)
-
-    part1 = len([i for i, _ in enumerate(blocks) if i not in supports])
-    for i, j in supports.items():
-        if all(any(x in v for k, v in supports.items() if k != i) for x in j):
-            part1 += 1
-
-    supported = {
-        b: {s for s, l in supports.items() if b in l}
-        for b in set[int].union(*supports.values())
-    }
-
-    count_falling = functools.partial(
-        _count_falling, supports=supports, supported=supported
+    bricks = sorted(
+        map(parse_brick, data),
+        key=lambda b: next(iter(b))[2] if len(b) > 2 else min(p[2] for p in b),
     )
 
-    part2 = sum(count_falling(i) for i in supports.keys())
+    supports, supported = make_support_graph(bricks)
+
+    part1 = sum(
+        all((len(supported.get(x, set())) != 1) for x in supports.get(i, set()))
+        for i in range(len(bricks))
+    )
+
+    part2 = sum(num_falling_bricks(i, supports, supported) for i in supports)
 
     return part1, part2
 

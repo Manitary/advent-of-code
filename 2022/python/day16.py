63b161e3e3d3b312c4867b38ccd007864dc477cc
Manitary <pak.ska@gmail.com>
2024-01-05 10:43:30 +0000

    Use a better algorithm for 2022/16

--- 2022/python/day16.py
+++ 2022/python/day16.py
@@ -1,104 +1,129 @@
-"""Solve Advent of Code Day 16 Year 2022."""
-
 import re
 from collections import defaultdict
-from functools import cache
-from itertools import product
-from typing import Callable
+from typing import NamedTuple
 
 from aocd import get_data, submit
 
-Dfs = Callable[[str, tuple[str, ...], int], float]
+DAY = 16
+YEAR = 2022
+
+EDGE_RE = re.compile(r"\d+|[A-Z]{2}")
+
+type Tunnel = tuple[str, str]
 
 
-def parse_input(
-    data: str,
-) -> tuple[tuple[str, ...], dict[str, int], dict[str, dict[str, float]]]:
-    """Return the valves and flows (if non-zero), and their travel distance."""
-    valves: dict[str, int] = {}
-    dists: dict[str, dict[str, float]] = defaultdict(
-        lambda: defaultdict(lambda: float("inf"))
+class Valve(NamedTuple):
+    rate: int
+    exits: list[str]
+
+
+def parse_valve(line: str) -> tuple[str, Valve]:
+    source, rate, *exits = EDGE_RE.findall(line)
+    return source, Valve(int(rate), exits)
+
+
+def parse_data(lines: list[str]) -> dict[str, Valve]:
+    return dict(map(parse_valve, lines))
+
+
+def find_distances(rooms: dict[str, Valve], key_rooms: set[str]) -> dict[Tunnel, int]:
+    """Return a map of distances key_room -> room."""
+    distances: dict[tuple[str, str], int] = {}
+    for start_room in key_rooms:
+        # BFS from start_room
+        queue: list[tuple[str, int]] = [(start_room, 1)]
+        while queue:
+            curr, d = queue.pop(0)
+            for new in rooms[curr].exits:
+                if (start_room, new) in distances:
+                    continue
+                distances[start_room, new] = d
+                queue.append((new, d + 1))
+
+    return distances
+
+
+def find_best_total_flow(
+    rooms: dict[str, Valve],
+    distances: dict[Tunnel, int],
+    start: str = "AA",
+    time: int = 30,
+) -> int:
+    """Return the best total flow, given the starting point and time left.
+
+    (The valve at the starting node has already been opened)"""
+    return max(
+        (
+            valve.rate * time_left
+            + find_best_total_flow(
+                {k: v for k, v in rooms.items() if k != start},
+                distances,
+                target,
+                time_left,
+            )
+            for target, valve in rooms.items()
+            if target != start
+            and (time_left := time - distances[start, target] - 1) > 0
+        ),
+        default=0,
     )
-    for row in data.split("\n"):
-        valve, *conns = re.findall(r"([A-Z]{2})", row)
-        flow_rate = int(re.findall(r"(\d+)", row)[0])
-        valves[valve] = flow_rate
-        dists[valve][valve] = 0
-        for other in conns:
-            dists[valve][other] = 1
-    for k, i, j in product(valves.keys(), repeat=3):
-        dists[i][j] = min(dists[i][j], dists[i][k] + dists[k][j])
-    # Filter out 0-flow valves from the list of valves to traverse.
-    flows = dict(filter(lambda v: v[1] > 0, valves.items()))
-    return tuple(flows.keys()), flows, dists
 
 
-def create_dfs(
-    flows: dict[str, int], distances: dict[str, dict[str, float]]
-) -> tuple[Dfs, Dfs]:
-    """Return the dfs functions to solve both parts with the given input."""
+def main() -> tuple[int, int]:
+    data = get_data(day=16, year=2022)
+    rooms = parse_data(data.splitlines())
+    key_rooms = {l: valve for l, valve in rooms.items() if valve.rate or l == "AA"}
+    distances = find_distances(rooms, set(key_rooms))
 
-    # Check all paths with memoisation.
-    # Whenever we reach a valve, we add all of its (future) total contribution.
-    @cache
-    def fun_1(
-        current_valve: str,
-        remaining_valves: tuple[str, ...],
-        time_left: int,
-    ) -> float:
+    part1 = find_best_total_flow(key_rooms, distances)
+
+    endpoints: dict[frozenset[str], int] = defaultdict(lambda: 0)
+
+    def find_and_record(
+        start: str = "AA",
+        cur_flow: int = 0,
+        time: int = 26,
+        seen: set[str] | None = None,
+    ) -> int:
+        seen = seen | {start} if seen else {start}
+
+        to_record = frozenset(seen - {"AA"})
+        endpoints[to_record] = max(endpoints[to_record], cur_flow)
+
         return max(
             (
-                flows[new_valve] * (time_left - distances[current_valve][new_valve] - 1)
-                + fun_1(
-                    current_valve=new_valve,
-                    remaining_valves=tuple(remaining_valves[:i])
-                    + tuple(remaining_valves[i + 1 :]),
-                    time_left=time_left - distances[current_valve][new_valve] - 1,
+                valve.rate * time_left
+                + find_and_record(
+                    target,
+                    cur_flow + valve.rate * time_left,
+                    time_left,
+                    seen,
                 )
-                for i, new_valve in enumerate(remaining_valves)
-                if distances[current_valve][new_valve] < time_left
+                for target, valve in key_rooms.items()
+                if target not in seen
+                and (time_left := time - distances[start, target] - 1) > 0
             ),
             default=0,
         )
 
-    # Check all paths with memoisation...again.
-    # When the candidates are unreachable in time, let the elephant go to them.
-    # There are some inefficiencies, for example we are ignoring the symmetry
-    # in the role of the player and the elephant.
-    @cache
-    def fun_2(
-        current_valve: str,
-        remaining_valves: tuple[str, ...],
-        time_left: int,
-    ) -> float:
-        candidates = tuple(
-            flows[new_valve] * (time_left - distances[current_valve][new_valve] - 1)
-            + fun_2(
-                current_valve=new_valve,
-                remaining_valves=tuple(remaining_valves[:i])
-                + tuple(remaining_valves[i + 1 :]),
-                time_left=time_left - distances[current_valve][new_valve] - 1,
-            )
-            for i, new_valve in enumerate(remaining_valves)
-            if distances[current_valve][new_valve] < time_left
-        )
-        if candidates:
-            return max(candidates)
-        return fun_1(
-            current_valve="AA", remaining_valves=remaining_valves, time_left=26
-        )
+    # Find all the best 26 minute flow rates
+    find_and_record()
 
-    return fun_1, fun_2
+    # Fill in all missing subsets of rooms
+    def fill_in_endpoints(cur: frozenset[str]) -> int:
+        if cur not in endpoints:
+            endpoints[cur] = max(fill_in_endpoints(cur - {e}) for e in cur)
+        return endpoints[cur]
 
+    fill_in_endpoints(frozenset(key_rooms.keys() - {"AA"}))
 
-def main() -> tuple[int, int]:
-    """Return the solution to part 1 and part 2."""
-    data = get_data(day=16, year=2022)
-    valves, flows, distances = parse_input(data)
-    dfs_part1, dfs_part2 = create_dfs(flows, distances)
-    part1 = dfs_part1("AA", valves, 30)
-    part2 = dfs_part2("AA", valves, 26)
-    return int(part1), int(part2)
+    # Check all the possible assignments of rooms
+    part2 = max(
+        flow + endpoints[frozenset(key_rooms.keys() - {"AA"} - human_work)]
+        for human_work, flow in endpoints.items()
+    )
+
+    return part1, part2
 
 
 if __name__ == "__main__":
